4756bdee7dc9569fe3f722fbffbe7590e0298036
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2024-10-19 05:49:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,45 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 2237
+BUFFER_SIZE = 1024
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+# Initialize and bind the UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    if len(data) == 35:
-        software = data[16:20].decode()
-        sw_version = data[28:].decode()
-        res = f'Software: {software} Version: {sw_version}'
-        print("35",res)
-    
-    elif len(data) == 69:
-        software = data[16:20].decode()
-        callee, caller, report = data[50:].decode().split(" ")
-        print("69", software, callee, caller, report)
+def handle_35_byte_message(data):
+    """Process and print a message with 35 bytes of data."""
+    software = data[16:20].decode()
+    sw_version = data[28:].decode()
+    result = f'Software: {software} Version: {sw_version}'
+    print("35-byte message:", result)
 
-    elif len(data) == 106:
-        print("Len:",len(data))
-        #print(data[0], data[1], data[2], data[3])
-        #print(data[4:15].decode())
-        software = data[16:21].decode()
-        #print(data[22:23].decode())
-        #print(data[23])
-        mesg = str.encode(data[29:64].decode()) 
-        print(mesg)
-        print("\n")
-    """if len(data) >= 69:
-        #for i in range(len(data)):
-        #    print(i, data[i])
-        #software = data[16:20].decode()
-        #db = data[20:22].decode()
-        #freq = data[23:30].decode()
-        #time = data[20:27].decode()
-        #print(data[20:].decode())
-        try:
-            print(data)
-            #mesg = data[50:].decode()
-            #print(mesg)
-        except:
-            pass
+def handle_69_byte_message(data):
+    """Process and print a message with 69 bytes of data."""
+    software = data[16:20].decode()
+    callee, caller, report = data[50:].decode().split(" ")
+    print("69-byte message:", software, callee, caller, report)
+
+def handle_106_byte_message(data):
+    """Process and print a message with 106 bytes of data."""
+    software = data[16:21].decode()
+    message = data[29:64].decode()
+    print(f"106-byte message - Software: {software}\nMessage: {message}\n")
+
+def process_data(data):
+    """Route the received data to the appropriate handler based on length."""
+    data_len = len(data)
+    
+    if data_len == 35:
+        handle_35_byte_message(data)
+    elif data_len == 69:
+        handle_69_byte_message(data)
+    elif data_len == 106:
+        handle_106_byte_message(data)
     else:
-        print(50*"*")
-"""
- 
+        print(f"Unhandled message length: {data_len}")
+
+# Main loop to receive and process data
+while True:
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    process_data(data)
